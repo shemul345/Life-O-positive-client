da6847efadc66424986c4f874217c54e0eef7062
shemul345 <shemulchowdhury7684@gmail.com>
2025-12-15 19:45:41 +0600

    added delete user functionality with successfully from database

--- src/Pages/Dashboard/Users/Users.jsx
+++ src/Pages/Dashboard/Users/Users.jsx
@@ -20,38 +20,134 @@ const Users = () => {
 
     const handleMakeAdmin = (user) => {
         const roleInfo = { role: 'admin' }
-        axiosSecure.patch(`/users/${user._id}`, roleInfo)
-            .then(res => {
-                console.log(res.data)
-                if (res.data.modifiedCount) {
-                    refetch()
-                    Swal.fire({
-                        title: "Congratulations!",
-                        text: `${user.displayName} marked as an Admin`,
-                        icon: "success",
-                        timer: 2000,
-                        showConfirmButton: false
-                    });
+
+        Swal.fire({
+                    title: "Are you sure?",
+                    text: `You have make ${user.displayName} as an Admin`,
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, Maked Admin"
+        })
+            .then(result => {
+                if (result.isConfirmed) {
+                    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+                        .then(res => {
+                            console.log(res.data)
+                            if (res.data.modifiedCount) {
+                                refetch()
+                                Swal.fire({
+                                    title: "Congratulations!",
+                                    text: `${user.displayName} marked as an Admin`,
+                                    icon: "success",
+                                    timer: 2000,
+                                    showConfirmButton: false
+                                });
+                            }
+                        })
+                }
+        })
+        
+    }
+
+    const handleMakeVolunteer = (user)=>{
+        const roleInfo = { role: 'volunteer' }
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: `You have make ${user.displayName} as an Volunteer`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Maked Admin"
+        })
+            .then(result => {
+                if (result.isConfirmed) {
+                    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+                        .then(res => {
+                            console.log(res.data)
+                            if (res.data.modifiedCount) {
+                                refetch()
+                                Swal.fire({
+                                    title: "Congratulations!",
+                                    text: `${user.displayName} marked as an Volunteer`,
+                                    icon: "success",
+                                    timer: 2000,
+                                    showConfirmButton: false
+                                });
+                            }
+                        })
                 }
             })
+
     }
+
     const handleRemoveDonor = (user) => {
         const roleInfo = { role: 'donor' }
-        axiosSecure.patch(`/users/${user._id}`, roleInfo)
-            .then(res => {
-                console.log(res.data)
-                if (res.data.modifiedCount) {
-                    refetch()
-                    Swal.fire({
-                        title: "Congratulations!",
-                        text: `${user.displayName} remove from Admin`,
-                        icon: "success",
-                        timer: 2000,
-                        showConfirmButton: false
-                    });
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: `You have make ${user.displayName} as a Donor`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Make Donor"
+        })
+            .then(result => {
+                if (result.isConfirmed) {
+                    axiosSecure.patch(`/users/${user._id}`, roleInfo)
+                        .then(res => {
+                            console.log(res.data)
+                            if (res.data.modifiedCount) {
+                                refetch()
+                                Swal.fire({
+                                    title: "Oops",
+                                    text: `${user.displayName} remove from Admin`,
+                                    icon: "error",
+                                    timer: 2000,
+                                    showConfirmButton: false
+                                });
+                            }
+                        })
                 }
-            })
+        })
+        
     }
+
+    const handleDeleteUser = (user) => {
+        Swal.fire({
+                    title: "Are you sure?",
+                    text:`You have deleted ${user.displayName} from database`,
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#3085d6",
+                    cancelButtonColor: "#d33",
+                    confirmButtonText: "Yes, delete it!"
+                })
+                    .then((result) => {
+                    if (result.isConfirmed) {
+        
+                        axiosSecure.delete(`/users/${user._id}`)
+                            .then(res => {
+                                console.log(res.data)
+                                if (res.data.deletedCount) {
+                                    refetch();
+                                    Swal.fire({
+                                        title: "Successfully",
+                                        text: `${user.displayName} has been deleted`,
+                                        icon: "success",
+                                        timer: 2000,
+                                        showConfirmButton:false
+                                    });
+                                }
+                            })
+                    }
+                });
+    }
+
     return (
         <div>
             <h1 className='text-2xl font-bold ml-10 mb-5'>All Users({users.length})</h1>
@@ -96,26 +192,34 @@ const Users = () => {
                                     {
                                         u.role === 'admin' ? <button
                                             onClick={() => handleRemoveDonor(u)}
-                                            className='btn btn-square bg-red-400 text-white mx-2'>
+                                            className='btn btn-square hover:bg-red-600 bg-red-400 text-white mx-2 tooltip'
+                                        data-tip='Remove Admin'>
                                             <TbShieldOff />
                                         </button> :
                                             <button
                                                 onClick={() => handleMakeAdmin(u)}
-                                                className='btn btn-square bg-green-500 text-white mx-2'>
+                                                className='btn btn-square hover:bg-green-800 bg-green-500 text-white mx-2 tooltip'
+                                                data-tip='Make Admin'>
                                                 <RiAdminFill />
                                             </button>
                                     }
 
                                     {
-                                        u.role === 'volunteer' ? <button className='btn btn-square hover:bg-primary hover:text-white mx-2'>
+                                        u.role === 'volunteer' ? <button
+                                            onClick={() => handleRemoveDonor(u)}
+                                            className='btn btn-square hover:bg-red-600 bg-red-400 text-white mx-2 tooltip'
+                                            data-tip='Remove Volunteer'>
                                             <MdRemoveDone />
                                         </button> :
-                                            <button className='btn btn-square hover:bg-primary hover:text-white mx-2'>
+                                            <button
+                                                onClick={() => handleMakeVolunteer(u)}
+                                                className='btn btn-square hover:bg-green-800 bg-green-500 text-white mx-2 tooltip'
+                                                data-tip='Make Volunteer'>
                                                 <MdVolunteerActivism />
                                             </button>
                                     }
                                     <button
-                                        // onClick={() => handleDeleteDonationRequest(request._id)}
+                                        onClick={() => handleDeleteUser(u)}
                                         className='btn btn-square hover:bg-primary hover:text-white'>
                                         <FaTrashAlt />
                                     </button>
